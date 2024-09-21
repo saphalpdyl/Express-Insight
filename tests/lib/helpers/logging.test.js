cd05b42a234c28b7614ddc342a1f6bd6f09ac2cb
saphalpdyl <saphalpdyl@gmail.com>
2024-09-21 16:57:44 -0500

    test-add: response log entry file

--- tests/lib/helpers/logging.test.js
+++ tests/lib/helpers/logging.test.js
@@ -19,26 +19,6 @@ describe('File logging tests', () => {
   let monthYearLogFolderPath;
   let monthYearString;
   let logFolderPath;
-
-  // const mockResponseInfo= {
-  //   requestId: mockRequestInfo.requestId,
-  //   responseId: 'mock-response-id',
-  //   timestamp: new Date().toISOString(),
-  //   url: '/api/example-endpoint',
-  //   statusCode: 200,
-  //   responseTime: Math.floor(Math.random() * 1000), // Random millisecond value
-  //   responseHeaders: {
-  //     'content-type': 'application/json',
-  //     'server': 'mock-server',
-  //     'x-powered-by': 'mock-framework',
-  //   },
-  //   headers: {
-  //     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-  //     contentType: 'application/json',
-  //     referer: 'https://example.com',
-  //     xForwardedFor: '192.168.1.1',
-  //   },
-  // };
   
   beforeAll(async () => {
     vol.reset();
@@ -123,6 +103,71 @@ describe('File logging tests', () => {
       xForwardedFor="192.168.1.1"
       queryParams={"param1":"value1","param2":"value2"}
       requestBody={}
+      "
+    `);
+  });
+
+  it("should create a log response entry", async () => {
+    const mockResponseInfo= {
+      requestId: '12345',
+      responseId: 'mock-response-id',
+      timestamp: new Date().toISOString(),
+      url: '/api/example-endpoint',
+      statusCode: 200,
+      responseTime: Math.floor(Math.random() * 1000), // Random millisecond value
+      responseHeaders: {
+        'content-type': 'application/json',
+        'server': 'mock-server',
+        'x-powered-by': 'mock-framework',
+      },
+      headers: {
+        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        contentType: 'application/json',
+        referer: 'https://example.com',
+        xForwardedFor: '192.168.1.1',
+      },
+    };
+    
+    const writeStream = generateLogFileStream(monthYearLogFolderPath, new Date());
+
+    const writeFnSpy = vi.spyOn(writeStream, "write");
+    
+    saveLogRequestEntry(mockResponseInfo, writeStream);
+    writeStream.close();
+
+    // Apparently write stream are asynchronous
+    // So wait for it to close before reading it
+    await once(writeStream, "close");
+
+    expect(writeFnSpy).toBeCalled();
+    expect(vol.existsSync(path.join(monthYearLogFolderPath, `1_1_2024.log`))).toBe(true);
+
+    const logContents = fs.readFileSync(path.join(monthYearLogFolderPath, `1_1_2024.log`)).toString("utf-8");
+    expect(logContents).toBeTruthy();
+    
+    expect(logContents).toMatchInlineSnapshot(`
+      "
+      <<<log.request>>>
+      [2024-01-01T06:00:00.000Z] 12345 GET /example
+      type=request
+      clientIp=192.168.1.1
+      userAgent="Mozilla/5.0"
+      contentType="application/json"
+      referer="https://example.com"
+      xForwardedFor="192.168.1.1"
+      queryParams={"param1":"value1","param2":"value2"}
+      requestBody={}
+
+      <<<log.request>>>
+      [2024-01-01T06:00:00.000Z] 12345 undefined /api/example-endpoint
+      type=request
+      clientIp=undefined
+      userAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+      contentType="application/json"
+      referer="https://example.com"
+      xForwardedFor="192.168.1.1"
+      queryParams=undefined
+      requestBody=undefined
       "
     `);
   });
