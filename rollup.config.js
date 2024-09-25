5e13bdba0ad8f87c79c7c9182f60883211c0368b
saphalpdyl <saphalpdyl@gmail.com>
2024-09-25 18:44:48 -0500

    fix: rollup config and tree shook the bundle configuration to reduce size from 1.8 MB to Kb range

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'lib/index.js',
+  treeshake: true,
   output: [
     {
       file: 'dist/bundle.cjs',
-      format: 'cjs'
+      format: 'cjs',
     },
     {
       file: 'dist/bundle.mjs',
@@ -15,7 +17,29 @@ export default {
   ],
   plugins: [
     resolve(),
-    commonjs()
+    commonjs(),
+    terser({
+      format: {
+        comments: false,
+      },
+      compress: {
+        dead_code: true,
+        drop_debugger: true,
+        conditionals: true,
+        evaluate: true,
+        booleans: true,
+        loops: true,
+        unused: true,
+        hoist_funs: true,
+        keep_fargs: false,
+        hoist_vars: true,
+        if_return: true,
+        join_vars: true,
+        side_effects: true,
+        warnings: false,
+      },
+      mangle: true,
+    }),
   ],
-  external: ['express', 'fs', 'path', 'util'],
+  external: ['express', 'fs', 'path', 'util', 'better-sqlite3', 'lodash', 'uuid', 'hbs'],
 };
